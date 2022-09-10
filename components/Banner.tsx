63182c1bff612acde46a8e2be071f60e66b85fef
Susheel Kumar <developer.susheel@gmail.com>
2022-09-11 00:55:48 +0530

    Blog Completed

---
+++ components/Banner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className="flex max-w-7xl mx-auto bg-green-500 rounded mt-5">
+      <div className="p-10 space-y-5 py-[100px]">
+        <h1 className="text-6xl max-w-xl font-serif">
+          <span className="underline decoration-black decoration-4">Better</span> 
+           &nbsp;place to write blog & connect</h1>
+          <h2>It is eaiser and free website to read content</h2>
+      </div>
+      <div></div>
+    </div>
+  )
+};
+
